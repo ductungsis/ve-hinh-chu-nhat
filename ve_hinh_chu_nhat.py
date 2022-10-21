6a317f23e88187d57109d424f67709453abd98ef
ductungsis <macair@MacAirs-MacBook-Air.local>
2022-10-21 21:30:07 +0700

    ve hinh chu nhat

---
+++ ve_hinh_chu_nhat.py
@@ -0,0 +1,28 @@
+import turtle
+
+turtle.pensize(5)
+
+
+t = turtle.Turtle()
+
+m = 100;
+n = 150;
+
+#ve canh dau tien
+t.forward(m)
+t.left(90)
+
+#ve canh thu hai
+t.forward(n)
+t.left(90)
+
+#ve canh thu ba
+t.forward(m)
+t.left(90)
+
+#ve canh thu tu
+t.forward(n)
+t.left(90)
+
+turtle.done()
+
